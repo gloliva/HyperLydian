b9c55b93b745bf015941eda86419f0acbbd98e9b
gloliva <gloliva@pm.me>
2023-06-05 12:35:36 -0700

    Remove unnecessary states.py file and move that logic into main

--- game/main.py
+++ game/main.py
@@ -3,8 +3,9 @@ import pygame as pg
 from pygame.locals import QUIT, RESIZABLE
 
 # project imports
-from defs import SCREEN_WIDTH, SCREEN_HEIGHT
-from states import GameState, transition_state
+from defs import SCREEN_WIDTH, SCREEN_HEIGHT, GameState
+from gameplay import run_gameplay
+from menus import run_main_menu
 
 # initial pygame setup
 pg.init()
@@ -41,7 +42,26 @@ def main():
         CLOCK.tick(60)
 
 
+def quit_game(game_clock: pg.time.Clock, main_screen: pg.Surface):
+    """Stop all game loops and quit game"""
+    print('Quitting the game')
+    pass
+
+
+# Game State transitions
+GAME_STATE_TO_LOOP_MAP = {
+    GameState.MAIN_MENU: run_main_menu,
+    GameState.GAMEPLAY: run_gameplay,
+    # GameState.DEATH_MENU: death_menu,
+    GameState.QUIT: quit_game,
+}
+
+
+def transition_state(next_state: GameState, game_clock: pg.time.Clock, main_screen: pg.Surface):
+    state_loop = GAME_STATE_TO_LOOP_MAP[next_state]
+    return state_loop(game_clock, main_screen)
+
+
 if __name__ == "__main__":
-    # maybe initialize stuff, like fonts
     main()
     pg.quit()
